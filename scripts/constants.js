d1a4ea5e0e2c5565212fcf48b47e14da2fd3664d
Lukas Schorn <lukasschorn@Lukass-MacBook-Pro.local>
2019-04-05 11:32:48 +0200

    fixed paths

--- scripts/constants.js
+++ scripts/constants.js
@@ -3,10 +3,10 @@ const GAME_HEIGHT = 550
 const ACCELERATION = 0.3
 
 function preload() {
-    hitHole = loadSound("../sounds/hit-hole.wav")
-    hitArc = loadSound("../sounds/arc-hit.wav")
-    gameOver = loadSound("../sounds/game-over.wav")
-    gameFont = loadFont("../font/novamono.ttf")
+    hitHole = loadSound("./sounds/hit-hole.wav")
+    hitArc = loadSound("./sounds/arc-hit.wav")
+    gameOver = loadSound("./sounds/game-over.wav")
+    gameFont = loadFont("./font/novamono.ttf")
     /* health2 = loadImage()
     health1 = loadImage()
     health0 = loadImage() */
